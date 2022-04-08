817769a506124cd6a73f9b715bcc010f8225211f
Nguyen Minh <minhnv2804@gmail.com>
2022-04-09 00:05:09 +0700

    commit

--- app.js
+++ app.js
@@ -38,15 +38,6 @@ const resolvers = require('./resolver/resolver');
     await server.start();
     server.applyMiddleware({ app });
     httpServer.listen(3000 , () =>
-        console.log(`Server ready at http://localhost:3000}`)
+        console.log(`Server ready at http://localhost:3000/graphql}`)
     );
 })();
-
-
-
-// server.start().then(res => {
-//     server.applyMiddleware({ app });
-//     app.listen({ port: 3000 }, () =>
-//         console.log(`Server ready at http://localhost:3000${server.graphqlPath}`)
-//     )
-// })
